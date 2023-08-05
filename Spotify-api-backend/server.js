ff69978c8b7ff7929579923c1fbafbe390ed2f58
Elisa Edson <e.edson@protonmail.ch>
2023-08-05 09:35:53 +0100

    working magic access key button

--- Spotify-api-backend/server.js
+++ Spotify-api-backend/server.js
@@ -1,21 +1,41 @@
 const express = require(`express`);
-const dotenv = require(`dotenv`);
+require("dotenv").config();
 const axios = require(`axios`);
 const cors = require(`cors`);
 const bp = require(`body-parser`);
 
-let port = process.env.port || 8222;
+let clientId = process.env.client_id;
+let clientSecret = process.env.client_secret;
 
+port = process.env.PORT || 8222;
 
 const API = `https://api.spotify.com/v1/search?q=`;
 let app = express();
 app.use(cors());
 app.use(bp.json());
 
+async function getAuth(){
+
+};
+
 
 app.get(`/`, async (request, response) =>{
     response.status(200).json(token);
   });
+
+app.get(`/auth`, async (request, response) =>{
+  const authParameters = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: 'grant_type=client_credentials&client_id='+clientId+'&client_secret='+clientSecret
+  }
+  fetch(`https://accounts.spotify.com/api/token`, authParameters)
+  .then(result => result.json())
+  .then(data => response.status(200).json(data))
+});
+
 
 app.listen(port, () =>{
   console.log(`listening on port:${port}`);
--- spotify-api-test/src/App.js
+++ spotify-api-test/src/App.js
@@ -2,24 +2,15 @@ import './App.css';
 import React from 'react';
 import axios from "axios";
 
-let clientId = process.env.client_id;
-let clientSecret = process.env.client_secret;
 
-  function App() {
+function App() {
 
-    async function getAuth(){
-      const authParameters = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: 'grant_type=client_credentials&client_id='+`1ad3f40124c94559a950bf36b234d9f3`+'&client_secret='+`DUMMYVALUE`
-      }
-      fetch(`https://accounts.spotify.com/api/token`, authParameters)
-      .then(result => result.json())
-      .then(data => console.log(data))
-    };
-    
+  async function getAuth(){
+    const API = "http://localhost:8222/auth"
+    const res =     await axios.get(API);
+    console.log(res);
+  }
+
     
     return(
       <div>
