7e9932b77cd741676841fc32a7cd9033623a870e
Elisa Edson <e.edson@protonmail.ch>
2023-08-05 14:58:47 +0100

    search function retrieves data

--- Spotify-api-backend/server.js
+++ Spotify-api-backend/server.js
@@ -9,7 +9,6 @@ let clientSecret = process.env.client_secret;
 
 port = process.env.PORT || 8222;
 
-const API = `https://api.spotify.com/v1/search?q=`;
 let app = express();
 app.use(cors());
 app.use(bp.json());
@@ -32,6 +31,19 @@ app.get(`/auth`, async (request, response) =>{
     body: 'grant_type=client_credentials&client_id='+clientId+'&client_secret='+clientSecret
   }
   fetch(`https://accounts.spotify.com/api/token`, authParameters)
+  .then(result => result.json())
+  .then(data => response.status(200).json(data))
+});
+
+app.post(`/search`, async (request, response) =>{
+  const API = `https://api.spotify.com/v1/search?q=`;
+  console.log(request.body.trackQuery);
+  const searchParameters = {
+    headers: {
+      Authorization: `Bearer ${request.body.key}`
+    }
+  }
+    fetch(API+request.body.trackQuery+`&type=track`, searchParameters)
   .then(result => result.json())
   .then(data => response.status(200).json(data))
 });
